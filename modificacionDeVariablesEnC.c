76d533c7118fa203905a5dc3c652104ee1c0d6dd
AngelDevSarrollo <160225178+AngelDevSarrollo@users.noreply.github.com>
2024-07-17 18:02:45 +0200

    Create modificacionDeVariablesEnC.c

---
+++ modificacionDeVariablesEnC.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	//1.Declaramos la variable
+	int miNumero;
+	//2. La inicializamos
+	miNumero = 10;
+	//3. Imprimimos la variable
+	// farmato %d -> decimal o int
+	printf("%d", miNumero);
+	
+	
+	return 0;
+}
+
